2b0f20c580cb456de1afb397eee65810da7ec742
Mykhailo <m_musienko@outlook.com>
2022-02-12 11:59:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+![GitHub code size in bytes](https://img.shields.io/github/languages/count/mypage-solutions/Lesson_14)
+![GitHub top language](https://img.shields.io/github/languages/top/mypage-solutions/Lesson_14)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/mypage-solutions/Lesson_14)
+![GitHub last commit](https://img.shields.io/github/last-commit/mypage-solutions/Lesson_14)
+
+# Homework #13 
+
+## Table of Contents
+
+- [General Info](#general-information)
+- [Technologies Used](#technologies-used)
+- [Project Status](#project-status)
+- [Contact](#contact)
+
+## General Information
+
+Homework contains a topic:
+1. Collections in Java. The Collection in Java is a framework that provides an architecture to store and manipulate the group of objects.
+   Java Collections can achieve all the operations that you perform on a data such as searching, sorting, insertion, manipulation, and deletion.
+
+![Example screenshot](https://github.com/mypage-solutions/Images/blob/main/Images/Collection-framework-hierarchy.jpg)
+![Example screenshot](https://github.com/mypage-solutions/Images/blob/main/Images/Collection-framework.jpeg)
+
+## Technologies Used
+
+- Java.
+
+## Project Status
+
+Project is: _complete_.
+
+## Contact
+
+Created by [Mykhailo.](https://github.com/mypage-solutions)
---
+++ src/com/mykhailo/PersonFactory.java
@@ -0,0 +1,38 @@
+package com.mykhailo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PersonFactory {
+
+    public static final Random RANDOM = new Random();
+    public static final String[] FIRST_NAME = {"Jack", "James", "Noah", "Daniel", "Conor", "Finn", "Harry", "Charlie"};
+    public static final String[] LAST_NAME = {"Duffy", "Brown", "Ryan", "Lynch", "Collins", "Quinn", "Walsh", "Connor"};
+
+    private List<Person> persons = new ArrayList<>();
+
+    public void addPersons(int number) {
+
+        for (int i = 0; i < number; i++) {
+            String firstName = FIRST_NAME[RANDOM.nextInt(FIRST_NAME.length)];
+            String lastName = LAST_NAME[RANDOM.nextInt(LAST_NAME.length)];
+            int age = RANDOM.nextInt(46) + 20;
+            int height = RANDOM.nextInt(51) + 150;
+            //  The formula is BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared.
+            //  A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9.
+            int bmi = RANDOM.nextInt(13) + 18;
+            double weight = Math.round(Math.pow(height / 100d, 2) * bmi * 100)/ 100d;
+            Person person = new Person(firstName, lastName, age, height, weight);
+            persons.add(person);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PersonFactory personList = new PersonFactory();
+        personList.addPersons(100);
+        System.out.println(personList.persons);
+
+    }
+}
